274531c62faec8bed8881f037a15c5bb3daaa207
chaiwat,ng <chaiwat.ng@kw.ac.th>
2018-12-05 12:53:56 +0700

    Hello

---
+++ lap2n1/lap2n1/Lap1.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Lap1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.lblnet = New System.Windows.Forms.Label()
+        Me.lbltax = New System.Windows.Forms.Label()
+        Me.lbltotal = New System.Windows.Forms.Label()
+        Me.btnclose = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.Lime
+        Me.btnCalculate.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(189, 56)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(71, 39)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "คำนวณ"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft YaHei UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(25, 63)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(91, 24)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblnet
+        '
+        Me.lblnet.AutoSize = True
+        Me.lblnet.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblnet.Location = New System.Drawing.Point(25, 132)
+        Me.lblnet.Name = "lblnet"
+        Me.lblnet.Size = New System.Drawing.Size(120, 22)
+        Me.lblnet.TabIndex = 2
+        Me.lblnet.Text = "เงินเดือนประจำปี"
+        '
+        'lbltax
+        '
+        Me.lbltax.AutoSize = True
+        Me.lbltax.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltax.Location = New System.Drawing.Point(25, 163)
+        Me.lbltax.Name = "lbltax"
+        Me.lbltax.Size = New System.Drawing.Size(68, 22)
+        Me.lbltax.TabIndex = 3
+        Me.lbltax.Text = "จ่ายภาษี"
+        '
+        'lbltotal
+        '
+        Me.lbltotal.AutoSize = True
+        Me.lbltotal.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltotal.Location = New System.Drawing.Point(25, 190)
+        Me.lbltotal.Name = "lbltotal"
+        Me.lbltotal.Size = New System.Drawing.Size(115, 22)
+        Me.lbltotal.TabIndex = 4
+        Me.lbltotal.Text = "เงินคงเหลือสุทธิ"
+        '
+        'btnclose
+        '
+        Me.btnclose.BackColor = System.Drawing.Color.Red
+        Me.btnclose.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnclose.Font = New System.Drawing.Font("Microsoft YaHei UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnclose.Location = New System.Drawing.Point(201, 210)
+        Me.btnclose.Name = "btnclose"
+        Me.btnclose.Size = New System.Drawing.Size(71, 39)
+        Me.btnclose.TabIndex = 5
+        Me.btnclose.Text = "ปิด"
+        Me.btnclose.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Label1.Font = New System.Drawing.Font("Microsoft YaHei", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(-1, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(285, 35)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "โปรแกรมคำนวณเงิน"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnclose)
+        Me.Controls.Add(Me.lbltotal)
+        Me.Controls.Add(Me.lbltax)
+        Me.Controls.Add(Me.lblnet)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Enabled = False
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents lblnet As Label
+    Friend WithEvents lbltax As Label
+    Friend WithEvents lbltotal As Label
+    Friend WithEvents btnclose As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ lap2n1/lap2n1/Lap1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Lap1
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles lbltotal.Click
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim txt As Double = TextBox1.Text
+        Dim num1 As Double
+        Dim num2 As Double
+        Dim num3 As Double
+
+        num1 = txt * 12
+        num2 = num1 * 0.05
+        num3 = num1 - num2
+
+        lblnet.Text = lblnet.Text + "  =  " + num1.ToString("###,###,###") + "  บาท"
+        lbltax.Text = lbltax.Text + "  =  " + num2.ToString("###,###,###") + "  บาท"
+        lbltotal.Text = lbltotal.Text + "  =  " + num3.ToString("###,###,###") + "  บาท"
+    End Sub
+
+    Private Sub btnclose_Click(sender As Object, e As EventArgs) Handles btnclose.Click
+        Me.Close()
+    End Sub
+End Class
